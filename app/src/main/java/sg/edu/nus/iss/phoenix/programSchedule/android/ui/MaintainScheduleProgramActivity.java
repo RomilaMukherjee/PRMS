82460572b1531e787135a02a43a8d6980abfb773
Ragupathy Rathinasabapathy <e0267619@u.nus.edu>
2018-09-18 10:39:28 +0800

    Program schedule structure ommit

---
+++ app/src/main/java/sg/edu/nus/iss/phoenix/programSchedule/android/ui/MaintainScheduleProgramActivity.java
@@ -0,0 +1,31 @@
+package sg.edu.nus.iss.phoenix.programSchedule.android.ui;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import sg.edu.nus.iss.phoenix.R;
+
+public class MaintainScheduleProgramActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maintain_schedule_program);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
