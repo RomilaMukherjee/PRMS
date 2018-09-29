03c859db6ba32649e831185419e343693cc6d1c4
Romila <ms.romila@gmail.com>
2018-09-29 20:26:59 +0530

    Added Context menu

--- app/src/main/java/sg/edu/nus/iss/phoenix/programSchedule/android/ui/ProgramSlotListActivity.java
+++ app/src/main/java/sg/edu/nus/iss/phoenix/programSchedule/android/ui/ProgramSlotListActivity.java
@@ -1,19 +1,26 @@
 package sg.edu.nus.iss.phoenix.programSchedule.android.ui;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
+
 import java.util.ArrayList;
 import java.util.List;
 import sg.edu.nus.iss.phoenix.R;
 import sg.edu.nus.iss.phoenix.core.android.controller.ControlFactory;
 import sg.edu.nus.iss.phoenix.programSchedule.entity.ProgramSlot;
+
 
 public class ProgramSlotListActivity extends AppCompatActivity {
 
@@ -21,6 +28,7 @@ public class ProgramSlotListActivity extends AppCompatActivity {
     private ListView slotList;
     private ProgramSlotAdapter slotAdapter;
     private ListView programSlotListView;
+    ArrayList<ProgramSlot> programSlotLst;
 
 
     @Override
@@ -28,29 +36,16 @@ public class ProgramSlotListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_program_slot_list);
         programSlotListView = (ListView) findViewById(R.id.radio_slot_list);
-        ArrayList<ProgramSlot> programSlotLst = new ArrayList<ProgramSlot>();
+        programSlotLst = new ArrayList<ProgramSlot>();
         slotAdapter = new ProgramSlotAdapter(this, programSlotLst);
         programSlotListView.setAdapter(slotAdapter);
+        registerForContextMenu(programSlotListView);
 
-        if(programSlotListView!=null) {
-            Log.d(TAG,"Inside programSlotListView when not null");
-            programSlotListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
-                @Override
-                public boolean onItemLongClick(AdapterView<?> adapterView, View view, int position, long l) {
-                    ProgramSlot programSlot = (ProgramSlot) adapterView.getItemAtPosition(position);
-                    Intent intent = new Intent(ProgramSlotListActivity.this, MaintainProgramSlotActivity.class);
-                    intent.putExtra("ProgramSlot", programSlot);
-                    startActivity(intent);
-                    return true;
-                }
-            });
-
-
-            FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab_create_program_slot);
+         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab_create_program_slot);
             fab.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    /**Create program Slot dummy object to copy and persist data**/
+                    //**Create program Slot dummy object to copy and persist data**//*
                     int annualYear = 2018;
                     String programName = "charity";
                     String dateInStr = "22/09/2018";
@@ -64,8 +59,63 @@ public class ProgramSlotListActivity extends AppCompatActivity {
                     ControlFactory.getMaintainScheduleController().copyProgramSlot(slotObj);
                 }
             });
-        }
+
     }
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v,ContextMenu.ContextMenuInfo menuInfo){
+       getMenuInflater().inflate(R.menu.main_context_menu_programslot, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+
+        final AdapterView.AdapterContextMenuInfo aInfo = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+        final int position = aInfo.position;
+        //Fetch the position of item in list
+        ProgramSlot slotObj = programSlotLst.get(aInfo.position);
+
+        // User clicked on a menu option in the app bar overflow menu
+        switch (item.getItemId()) {
+            // Respond to a click on the "Copy" menu option
+            case R.id.copy_slot_menu_item:
+                if (slotObj == null) {
+                    // Prompt for the selection of a radio program.
+                    Toast.makeText(this, "Select a program slot", Toast.LENGTH_SHORT).show();
+                    Log.v(TAG, "There is no selected radio program.");
+                }
+                else {
+                    Log.v(TAG, "Viewing radio program: " + slotObj.getProgramName()+ "...");
+
+                    ControlFactory.getMaintainScheduleController().copyProgramSlot(slotObj);
+                }
+                // Respond to a click on the "Copy" menu option
+            case R.id.edit_slot_menu_item:
+                if (slotObj == null) {
+                    // Prompt for the selection of a radio program.
+                    Toast.makeText(this, "Select a program slot to edit", Toast.LENGTH_SHORT).show();
+                    Log.v(TAG, "There is no selected radio program.");
+                }
+                else {
+                    Log.v(TAG, "Viewing radio program: " + slotObj.getProgramName()+ "...");
+                    //ControlFactory.getMaintainScheduleController().selectEditProgramSlot(progSelectedSlot);
+                }
+            case R.id.delete_slot_menu_item:
+                if (slotObj == null) {
+                    // Prompt for the selection of a radio program.
+                    Toast.makeText(this, "Select a program slot to delete", Toast.LENGTH_SHORT).show();
+                    Log.v(TAG, "There is no selected radio program.");
+                }
+                else {
+                    Log.v(TAG, "Viewing radio program: " + slotObj.getProgramName()+ "...");
+                    //ControlFactory.getProgramController().selectDeleteProgramSlot(progSelectedSlot);
+                }
+
+        }
+
+        return true;
+    }
+
+
 
     @Override
     protected void onPostCreate(@Nullable Bundle savedInstanceState) {
@@ -79,7 +129,7 @@ public class ProgramSlotListActivity extends AppCompatActivity {
         slotAdapter.clear();
         Log.v(TAG, "List of Slots is :" + programSlots.size());
         for (int i = 0; i < programSlots.size(); i++) {
-            Log.d(TAG,"Indise program slot.."+programSlots.get(i).getProgramName());
+            Log.d(TAG,"Inside program slot.."+programSlots.get(i).getProgramName());
             slotAdapter.add(programSlots.get(i));
         }
     }
