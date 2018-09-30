df1e711a1a09f19d24aba9da134ec1a48719bc69
Romila <ms.romila@gmail.com>
2018-09-30 22:01:02 +0530

    Corrected the JSOn fields name for programSlotName

--- app/src/main/java/sg/edu/nus/iss/phoenix/programSchedule/android/delegate/RetrieveProgramSlotDelegate.java
+++ app/src/main/java/sg/edu/nus/iss/phoenix/programSchedule/android/delegate/RetrieveProgramSlotDelegate.java
@@ -85,7 +85,7 @@ public class RetrieveProgramSlotDelegate extends AsyncTask<String, Void, String>
                 for (int i = 0; i < psArray.length(); i++) {
                     JSONObject asJson = psArray.getJSONObject(i);
                     String duration = asJson.getString("duration");
-                    String programName = asJson.getString("programName");
+                    String programName = asJson.getString("programSlotName");
                     String dateofProgram = asJson.getString("dateofProgram");
                     String startTime = asJson.getString("startTime");
                     String weekStartDate = asJson.getString("weekStartDate");
