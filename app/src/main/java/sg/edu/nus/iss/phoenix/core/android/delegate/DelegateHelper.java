f3420e59839f22df220aba4b634cc18783c0d527
Shashwat Jain <shashwatjain@icloud.com>
2018-09-30 20:21:43 +0800

    Added Delete user function on List Screen

--- app/src/main/java/sg/edu/nus/iss/phoenix/core/android/delegate/DelegateHelper.java
+++ app/src/main/java/sg/edu/nus/iss/phoenix/core/android/delegate/DelegateHelper.java
@@ -2,8 +2,10 @@ package sg.edu.nus.iss.phoenix.core.android.delegate;
 
 public class DelegateHelper {
     // RESTful parameters.
-    public final static String PRMS_BASE_URL_AUTHENTICATE = "http://172.17.16.200:8080/phoenix/rest/Login/doLogin?";
-    public final static String PRMS_BASE_URL_RADIO_PROGRAM = "http://172.17.16.200:8080/phoenix/rest/radioprogram";
-    public final static String PRMS_BASE_URL_PROGRAM_SCHEDULE = "http://172.17.16.200:8080/phoenix/rest/programschedule";
-    public final static String PRMS_BASE_URL_USER = "http://172.17.16.200:8080/phoenix/rest/users";
+    public final static String PRMS_BASE_URL = "http://192.168.1.15:8080/phoenix/rest/";
+    public final static String PRMS_BASE_URL_AUTHENTICATE = PRMS_BASE_URL + "Login/doLogin?";
+    public final static String PRMS_BASE_URL_RADIO_PROGRAM = PRMS_BASE_URL + "radioprogram";
+    public final static String PRMS_BASE_URL_PROGRAM_SCHEDULE = PRMS_BASE_URL + "programschedule";
+    public final static String PRMS_BASE_URL_USER = PRMS_BASE_URL + "users";
+    public final static String PRMS_BASE_URL_DELETE_USER = PRMS_BASE_URL + "users/delete";
 }
